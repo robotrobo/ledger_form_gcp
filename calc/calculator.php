e566e34334350746d941ec12f50f739423c8c649
anish agrawal <kanhaagrawal0804@gmail.com>
2019-04-03 15:04:49 +0530

    fixing cal.php

--- calc/calculator.php
+++ calc/calculator.php
@@ -6,8 +6,7 @@ $fa2 = $_GET("fa2");
 $term1 = $_GET("term1");
 $term2 = $_GET("term2");
 
-
-$string = ('py.exe .\percentage_cal.py '.escapeshellarg($fa1)." ".escapeshellarg($fa2)." ".escapeshellarg($term1)." ".escapeshellarg($term2));
+$string = ('py.exe C:\inetpub\wwwroot\calc\percentage_cal.py '.escapeshellarg($fa1)." ".escapeshellarg($fa2)." ".escapeshellarg($term1)." ".escapeshellarg($term2));
 $ans = shell_exec($string);
 echo $ans;
 
