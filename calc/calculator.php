be1253ac55c98e91693fc783da4dcffdb4903031
anish agrawal <kanhaagrawal0804@gmail.com>
2019-04-03 15:21:30 +0530

    bug fixes

--- calc/calculator.php
+++ calc/calculator.php
@@ -1,10 +1,10 @@
 
 
 <?php 
-$fa1 = $_GET("fa1");
-$fa2 = $_GET("fa2");
-$term1 = $_GET("term1");
-$term2 = $_GET("term2");
+$fa1 = $_GET["fa1"];
+$fa2 = $_GET["fa2"];
+$term1 = $_GET["term1"];
+$term2 = $_GET["term2"];
 
 $string = ('py.exe C:\inetpub\wwwroot\calc\percentage_cal.py '.escapeshellarg($fa1)." ".escapeshellarg($fa2)." ".escapeshellarg($term1)." ".escapeshellarg($term2));
 $ans = shell_exec($string);
