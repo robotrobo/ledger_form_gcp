cf77d356a92266d7b2432a286260c1dedf544337
anish agrawal <kanhaagrawal0804@gmail.com>
2019-04-03 15:14:18 +0530

    modifying calc.js

--- calc/calculator.js
+++ calc/calculator.js
@@ -14,7 +14,9 @@ function setup() {
     answer = select('#answer');
     button = select('#submit');
     button.mousePressed(_ => {
-        result = httpGet(`calculator.php?fa1=${fa1.value()}&fa2=${fa2.value()}&term1=${term1.value()}&term2=${term2.value()}`)
+        httpGet(`calculator.php?fa1=${fa1.value()}&fa2=${fa2.value()}&term1=${term1.value()}&term2=${term2.value()}`, (res) => {
+            result = res
+        })
         answer.value(result)
     })
 }
