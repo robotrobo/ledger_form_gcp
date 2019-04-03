6bde5ad9817ea1884a6b52cbc05d6f7f90e65bda
anish agrawal <kanhaagrawal0804@gmail.com>
2019-04-03 14:54:22 +0530

    updating calc

--- calc/calculator.js
+++ calc/calculator.js
@@ -3,9 +3,9 @@ let fa2;
 let term1;
 let term2;
 let result;
-let answer;
+let answer, button;
 
-function sketch() {
+function setup() {
 
     fa1 = select('#fa1');
     fa2 = select('#fa2');
@@ -13,8 +13,10 @@ function sketch() {
     term2 = select('#term2');
     answer = select('#answer');
     button = select('#submit');
-    button.mouseClicked(_ => {
+    button.mousePressed(_ => {
         result = httpGet(`calculator.php?fa1=${fa1.value()}&fa2=${fa2.value()}&term1=${term1.value()}&term2=${term2.value()}`)
         answer.value(result)
     })
 }
+
+function draw() {}
