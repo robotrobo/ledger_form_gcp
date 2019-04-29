62132f547a557fbe06d9a44a2c97794635ff7c2f
anish agrawal <kanhaagrawal0804@gmail.com>
2019-04-29 12:57:45 +0530

    Updating date

--- import_ledger.py
+++ import_ledger.py
@@ -1,12 +1,11 @@
-# # is requested by the php file to call tally and ask for data
+# is requested by the php file to call tally and ask for data
 import requests
 import sys
 if len(sys.argv) == 2:
 
     name = sys.argv[1]
-    req = '<ENVELOPE > <HEADER > <TALLYREQUEST > Export Data < /TALLYREQUEST > </HEADER > <BODY > <EXPORTDATA > <REQUESTDESC > <STATICVARIABLES > <!--Specify the FROM DATE here--> < SVFROMDATE > 20080401 < /SVFROMDATE > <!--Specify the TO DATE here--> < SVTODATE > 20190331 < /SVTODATE > <SVEXPORTFORMAT >$$SysName: ASCII < /SVEXPORTFORMAT > <!--Specify the LedgerName here--> < LEDGERNAME >{}< /LEDGERNAME > </STATICVARIABLES > <!--Report Name--> < REPORTNAME > Ledger Vouchers < /REPORTNAME > </REQUESTDESC > </EXPORTDATA > </BODY > </ENVELOPE >'.format(
+    req = '<ENVELOPE > <HEADER > <TALLYREQUEST > Export Data < /TALLYREQUEST > </HEADER > <BODY > <EXPORTDATA > <REQUESTDESC > <STATICVARIABLES > <!--Specify the FROM DATE here--> < SVFROMDATE > 20080401 < /SVFROMDATE > <!--Specify the TO DATE here--> < SVTODATE > 20200331 < /SVTODATE > <SVEXPORTFORMAT >$$SysName: ASCII < /SVEXPORTFORMAT > <!--Specify the LedgerName here--> < LEDGERNAME >{}< /LEDGERNAME > </STATICVARIABLES > <!--Report Name--> < REPORTNAME > Ledger Vouchers < /REPORTNAME > </REQUESTDESC > </EXPORTDATA > </BODY > </ENVELOPE >'.format(
         name)
-    # req = req.replace('&','&amp;')
     r = requests.post("http://anishfoods.in:9000", data=req)
     # r = requests.post("http://localhost:9000", data=req)
     if r.text == "":
